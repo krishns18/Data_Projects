888d76561df3e57021e1209f12c6e23914b0da36
SHAILESH KRISHNA <10409934+krishns18@users.noreply.github.com>
2020-09-16 20:17:53 -0700

    Added etl.py file

---
+++ RedShift_Datawarehouse_Implementation/etl.py
@@ -0,0 +1,41 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    """
+    Load data from files stored in s3 to staging tables using queries declared on sql_queries script
+    """
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    """
+    Select and transform data from staging tables into the dimension tables using the queries declared on the sql_queries script
+    """
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    Extract songs metadata and user activity data from S3, transform it using a staging table, and load it into dimension tables for analysis
+    """
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
